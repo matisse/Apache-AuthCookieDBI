b6dd08c54a3cfaf09532a6e81298c003d6e3c62f
jacob <jacob>
2003-10-10 20:20:42 +0000

    *** empty log message ***

--- lib/Apache2/AuthCookieDBI.pm
+++ lib/Apache2/AuthCookieDBI.pm
@@ -1,3 +1,4 @@
+
 #===============================================================================
 #
 # Apache::AuthCookieDBI
@@ -29,7 +30,7 @@ package Apache::AuthCookieDBI;
 use strict;
 use 5.004;
 use vars qw( $VERSION );
-( $VERSION ) = '$Revision: 1.7 $' =~ /([\d.]+)/;
+( $VERSION ) = '$Revision: 1.8 $' =~ /([\d.]+)/;
 
 use Apache::AuthCookie;
 use vars qw( @ISA );
@@ -101,7 +102,7 @@ Apache::AuthCookieDBI - An AuthCookie module backed by a DBI database.
 
 =head1 VERSION
 
-	$Revision: 1.7 $
+	$Revision: 1.8 $
 
 =head1 SYNOPSIS
 
