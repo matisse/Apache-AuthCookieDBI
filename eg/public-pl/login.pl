8514d2b0b4ccbb2ba738d0559fdd917963444d93
jacob <jacob>
2003-10-10 22:33:31 +0000

    *** empty log message ***

--- eg/public-pl/login.pl
+++ eg/public-pl/login.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -Tw
 #
-# $Id: login.pl,v 1.2 2003/10/10 20:53:26 jacob Exp $
+# $Id: login.pl,v 1.3 2003/10/10 22:33:31 jacob Exp $
 #
 # Display a login form with hidden fields corresponding to the page they
 # wanted to see.
@@ -21,11 +21,12 @@ if ( $r->prev() ) { # we are called as a subrequest.
 	             : $r->prev()->uri();
 	$authcookiereason = $r->prev()->subprocess_env( 'AuthCookieReason' );
 } else {
-	$destination = $r->args( 'destination' );
-	$authcookiereason = $r->args( 'AuthCookieReason' );
+        my %args = $r->args;
+	$destination = $args{ 'destination' };
+	$authcookiereason = $args{ 'AuthCookieReason' };
+        $t->add_tag( CREDENTIAL_0 => $r->prev->args('credential_0');
 }
 $t->add_tag( DESTINATION => $destination );
-$t->add_tag( CREDENTIAL_0 => $r->prev->args('credential_0');
 
 unless ( $authcookiereason eq 'bad_cookie' ) {
 	$t->template_file( "../html/login.html" );
